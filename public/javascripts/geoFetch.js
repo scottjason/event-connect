14b7bb30c8fdc6eef1fc6e5ed65291b9b0ff9e6c
scottjason <scottleejason@gmail.com>
2014-09-08 02:13:49 -0700

    adds google maps api, implements reverse geo

--- public/javascripts/geoFetch.js
+++ public/javascripts/geoFetch.js
@@ -4,13 +4,25 @@ function GeoFetch(){
   this.socket = io.connect();
 }
 
-GeoFetch.prototype.collect = function() {
-   navigator.geolocation.getCurrentPosition( this.callback.bind( this ) );
+GeoFetch.prototype.initialize = function() {
+  if ( navigator.geolocation && typeof ( navigator.geolocation.getCurrentPosition ) == "function") {
+       navigator.geolocation.getCurrentPosition( this.geoCallback.bind( this ) );
+  }
 }
 
-GeoFetch.prototype.callback = function( position ) {
+GeoFetch.prototype.geoCallback = function( position ) {
   this.socket.emit( 'geoPass', position.coords.latitude, position.coords.longitude );
-  this.openSockets();
+  var latLng = new google.maps.LatLng( position.coords.latitude, position.coords.longitude );
+  var coder = new google.maps.Geocoder();
+      coder.geocode( { 'latLng': latLng }, this.reverseGeoCallback.bind( this ) );
+}
+
+GeoFetch.prototype.reverseGeoCallback = function( results, status ) {
+   if ( status == google.maps.GeocoderStatus.OK ) {
+      var userLocation = results[1].formatted_address;
+   }
+   console.log( userLocation );
+   this.openSockets();
 }
 
 GeoFetch.prototype.openSockets = function() {
@@ -20,10 +32,8 @@ GeoFetch.prototype.openSockets = function() {
 }
 
 var GeoFetch = new GeoFetch;
-GeoFetch.collect();
+GeoFetch.initialize();
 })
-
-
 
 
 
