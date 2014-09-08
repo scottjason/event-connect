fceb5b11e690cf5ffae48bb39747bb795a8160ea
scottjason <scottleejason@gmail.com>
2014-09-08 06:03:07 -0700

    adds fixed table header

--- public/javascripts/geoResult.js
+++ public/javascripts/geoResult.js
@@ -2,31 +2,19 @@ function GeoResult(){}
 
 GeoResult.prototype.initialize = function( socket ) {
   socket.on('geoResult', function( result ) {
-  this.clean( result, this.filter.bind( this ) )
+  this.clean( result, this.renderGeoEvents.bind( this ) )
   }.bind( this ));
 }
 
 GeoResult.prototype.clean = function( result, callback ) {
   var response = JSON.parse(result);
-  callback( response, this.render );
-}
-
-GeoResult.prototype.filter = function( response, callback ) {
-    // var filteredArr = [];
-    // for( var i = 0; i < response.event.length; i++ ){
-    // filteredArr.push( _.pick( response.event[i], 'artists', 'image', 'startDate', 'tags', 'title', 'url', 'venue' ) );
-
   callback( response );
 }
 
-GeoResult.prototype.render = function( response ) {
-// var startDate = _.pluck(arr, 'startDate');
-// console.log( response )
-
-var template = $('#geoTemplate').html();
-var output = Mustache.render(template, response);
-$('#geoTarget').append(output);
-
+GeoResult.prototype.renderGeoEvents = function( response ) {
+  var template = $('#geoTemplate').html();
+  var output = Mustache.render( template, response );
+  $('#geoTarget').append( output );
 }
 
 var GeoResult = new GeoResult;
