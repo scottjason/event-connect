2683073bae3516d9c3034f2089680a4dc87c262e
scottjason <scottleejason@gmail.com>
2014-09-07 17:34:38 -0700

    completes JSON iteration, adds callbacks to control flow

--- public/javascripts/geoResult.js
+++ public/javascripts/geoResult.js
@@ -1,4 +1,5 @@
-function GeoResult(){}
+function GeoResult(){
+}
 
 GeoResult.prototype.listen = function( socket ) {
   socket.on('geoResult', function( result ) {
@@ -7,15 +8,32 @@ GeoResult.prototype.listen = function( socket ) {
 }
 
 GeoResult.prototype.clean = function( result ) {
-  var response = JSON.parse(result)
-  console.log(response)
+  var response = JSON.parse(result);
+  this.prep( response, this.filter );
 }
 
-GeoResult.prototype.filter = function() {}
+GeoResult.prototype.prep = function( response, callback ) {
+  var resultsArr = [];
+  resultsArr.push( response.event );
+  callback( resultsArr, this.render );
+}
 
-GeoResult.prototype.render = function() {}
+GeoResult.prototype.filter = function( resultsArr, callback ) {
+  var property = ['artist', 'image', 'startDate', 'title', 'url', 'venue'];
+  var filteredArr = [];
+
+  for (var i = 0; i < resultsArr.length; i++) {
+    for ( property in resultsArr[i] ) {
+      if ( !resultsArr[i].hasOwnProperty( property ) ) continue;
+
+        filteredArr.push( resultsArr[i][property] );
+    }
+  }
+  callback( filteredArr );
+}
+
+GeoResult.prototype.render = function(arr) {
+  console.log(arr)
+}
 
 var GeoResult = new GeoResult;
-
-
-
